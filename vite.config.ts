7ae8ffa6f76bf505a618e4795585ad888dbf8567
Albert Stoykov <albert.stoykov@gmail.com>
2025-12-09 11:56:53 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: "/The-Badge-Scanner-Website-V2/",
+    base: "/The-Badge-Scanner-Website---V2/",
     plugins: [react()],
     resolve: {
       extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
