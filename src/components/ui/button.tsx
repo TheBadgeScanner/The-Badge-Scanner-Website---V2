0e47955ea2a5f3012fcafabd045f9c127b1422b7
Albert Stoykov <albert.stoykov@gmail.com>
2025-12-10 10:28:54 +0000

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        blue: "border border-solid !bg-[#2563eb] !text-white hover:!bg-[#1d4ed8] !border-[#2563eb] hover:!border-[#1d4ed8]",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
